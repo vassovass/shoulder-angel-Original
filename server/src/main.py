972f1698061dea685dee4c0d2cf545775319104b
Sam Stowers <phantom4d@gmail.com>
2024-07-20 22:25:25 -0700

    Add initial schedule check functionality

--- server/src/main.py
+++ server/src/main.py
@@ -1,20 +1,63 @@
 import datetime
-from fastapi import FastAPI
+import requests
 from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import weave
+from dotenv import load_dotenv
 
-from apscheduler.schedulers.background import BackgroundScheduler
+load_dotenv()
 
-# from apscheduler import apscheduler
+# local
+from src.models import GroqScheduler
+from src.voice import call_user
+
+weave.init("shoulder-angel")
+
+scheduler = GroqScheduler(
+    model="llama3-70b-8192",
+    system_message="Your role is to check whether the user is working when they should be. Compare their stated schedule with the current time.",
+)
 
 
 def printit():
     print(datetime.datetime.now())
 
 
+@weave.op()
+async def check_schedule():
+    "Check if the user is active, also check their schedule. If scheduled but not active, place call"
+
+    # fetch setting for work schedule (string initially)
+    # gonna do a string literal for testing purposes
+    user_sched_str = "I want to work literally all the time"
+
+    # get current timestamp & format as something LLM readable
+    # e.g. "Monday August 3rd at 5pm"
+    now = datetime.datetime.now()
+    llmnow = now.strftime("%A %B %d at %I%p PST")
+
+    # check backend to see if user has been active (program sending pings)
+    user_last_active = now
+    llm_user_last_active = llmnow
+
+    # have LLM check if it's within schedule or not
+    res = await scheduler.predict(user_sched_str, llmnow)
+
+    print(res)
+
+    print(res == "True")
+    # if inactive and also outside of schedule, trigger call
+    if res == "True":
+        call_user()
+
+    return res
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(printit, "interval", seconds=5)
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(check_schedule, "interval", seconds=60)
     scheduler.start()
     yield
 
