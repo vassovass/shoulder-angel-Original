ca52956e9a04275d8ffd53b3de1fa47e2ad8dd19
Sam Stowers <phantom4d@gmail.com>
2024-07-21 01:19:07 -0700

    Fix client script

--- client/main.py
+++ client/main.py
@@ -4,34 +4,36 @@ import json
 
 # function to query the local screen-pipe operation.
 
+sp_url = "http://localhost:3030"
+backend_url = "http://localhost:8000"
+
 
 def get_screenpipe_activity():
     """Get latest OCR info from screenpipe"""
     # query most recent OCR from screenpipe
-    sp_url = "https://localhost:3030"
-    backend_url = "https://localhost:8000"
 
-    res = requests.get(f"{sp_url}/search?limit=1&offset=0&content_type=ocr")
+    res = requests.get(f"{sp_url}/search?limit=1&offset=0&content_type=ocr").json()
 
     try:
-        res_json = json.loads(res)
+        res_json = res
+        return res_json
     except Exception as e:
         print(e)
+        return None
 
-    ocr_content_str: str = recent_ocr["data"][0]["content"]["text"]
+    # ocr_content_str: str = recent_ocr["data"][0]["content"]["text"]
 
-    print(ocr_content_str)
+    # print(ocr_content_str)
 
     # if query bounces, assume user isn't on computer
 
     # run OCR through
-    return ocr_json
 
 
 def main():
     """Run query on an interval"""
 
-    interval = 60  # seconds
+    interval = 120  # seconds
 
     while True:
         res = get_screenpipe_activity()
@@ -41,3 +43,6 @@ def main():
         time.sleep(interval)
 
     return None
+
+
+main()
